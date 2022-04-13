d4f85e9fcb8ed752046efb61d829579fa506dd21
KouSei089 <qq00891829@icloud.com>
2022-04-14 00:11:21 +0900

    Add:tailwindcssの導入

--- src/App.js
+++ src/App.js
@@ -9,59 +9,31 @@ const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <>
+    <div className="bg-green-300">
       <Layout>
-        <Header style={{
-                color: 'black',
-                backgroundColor: 'white',
-              }}>
-        </Header>
-        <Content
-          style={{
-            fontSize: '96px',
-            backgroundColor: 'white',
-            textAlign: 'center',
-            fontFamily: 'Alegreya Sans SC',
-            fontWeight: 400,
-            letterSpacing: 10
-          }}
-        >Izumi Haruya
-        </Content>
-        <Footer style={{backgroundColor: 'white'}}></Footer>
+        <Header className="bg-white"></Header>
+        <Content className="bg-white text-7xl text-center font-thin tracking-wider font-ale">Izumi Haruya</Content>
+        <Footer className="bg-white"></Footer>
       </Layout>
       <Layout>
-        <Header
-          style={{
-            backgroundColor: 'white',
-            fontSize: '36px',
-            fontFamily: 'Alegreya Sans SC',
-            fontWeight: 400,
-            letterSpacing: 5,
-          }}
-        >
+        <Header className="bg-white text-2xl font-ale font-thin tracking-wide">
           Profile
         </Header>
-        <Content
-          style={{
-            backgroundColor: '#f7f3ec',
-            fontFamily: 'Zen Kurenaido',
-            padding: '96px',
-            paddingTop: '20px',
-            paddingBottom: 0,
-            lineHeight: '30px',
-          }}
-        >
-        手アパレルブランドで5年間の実務を経験。プログラミングの独学・読書を通じてテクノロジーでの問題解決を知り、エンジニアになりたいと考え、2021年4月よりプログラミングスクールRUNTEQへ入学。その後『Mikke（ミッケ）習慣ジャーナリング』を個人開発し、リリースする。月間1500PV以上 / UU200人以上を達成しました。
-        現在は、React/TypeScriptを使用したアプリを作成中です。
-        個人活動では、オンラインコミュニティ『DIALogue』を運営おります。
+        <Content className="bg-natural font-zen leading-5">
+        	大手アパレルブランドで5年間の実務を経験。プログラミングの独学・読書を通じてテクノロジーでの問題解決を知り、エンジニアになりたいと考え、2021年4月よりプログラミングスクールRUNTEQへ入学。その後『Mikke（ミッケ）習慣ジャーナリング』を個人開発し、リリースする。月間1500PV以上 / UU200人以上を達成しました。
+          現在は、React/TypeScriptを使用したアプリを作成中です。
+          個人活動では、オンラインコミュニティ『DIALogue』を運営おります。
         </Content>
         <Row type="flex" justify="space-around"  style={{ padding: '10px', backgroundColor: '#f7f3ec' }}>
           <Col span={8}><Cards /></Col>
           <Col span={8}><Cards /></Col>
           <Col span={8}><Cards /></Col>
         </Row>
+        <h1 className="text-3xl font-bold underline text-green-500">
+          Hello world!
+        </h1>
     </Layout>
-    </>
+    </div>
   );
 }
 
