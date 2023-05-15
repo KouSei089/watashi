a4da0a872d811b1a35c3fee9a5d23b716785b88c
KouSei089 <qq00891829@icloud.com>
2023-05-15 21:51:09 +0900

    fix:カラーをwhiteに統一した

--- src/HistoryTimeTree.js
+++ src/HistoryTimeTree.js
@@ -17,10 +17,10 @@ import { RiGitRepositoryFill } from "react-icons/ri";
 const HistoryTimeTree = () => {
   return (
     <Timeline mode="alternate">
-      <Timeline.Item color="gold">
+      <Timeline.Item color="#F4F4F2">
         <p className="font-black border-matte border-b-2">TeamLab Backend Engineer. 2022-06-15 Now in ...</p>
       </Timeline.Item>
-      <Timeline.Item color="purple">
+      <Timeline.Item color="#F4F4F2">
         <p>Create a <a href="https://kousei089.github.io/portfolio-izuha/" target="_blank" rel="noopener noreferrer">"portfolio website."</a> 2022-04-14</p>
         <p>- Technology stacks used -</p>
         <div className="flex justify-end">
@@ -33,9 +33,9 @@ const HistoryTimeTree = () => {
           </div>
         </a>
       </Timeline.Item>
-      <Timeline.Item color="purple">I studied mainly Ruby on Rails at "RUNTEQ", a programming school run by Startup Technology Inc. As for personal development, I released "Mikke," a service that digitally realizes "writing," analyzes emotions, and supports users to easily make it a habit. We used AWS for deployment.
+      <Timeline.Item color="#F4F4F2">I studied mainly Ruby on Rails at "RUNTEQ", a programming school run by Startup Technology Inc. As for personal development, I released "Mikke," a service that digitally realizes "writing," analyzes emotions, and supports users to easily make it a habit. We used AWS for deployment.
       Currently, we are working on an application using React and TypeScript.</Timeline.Item>
-      <Timeline.Item color="purple">
+      <Timeline.Item color="#F4F4F2">
         <p>Released <a href="https://a-mikke.com" target="_blank" rel="noopener noreferrer">"Mikke", a personal development service.</a> 2022-02-14</p>
         <p>- Technology stacks used -</p>
         <div className="flex justify-end">
@@ -48,20 +48,20 @@ const HistoryTimeTree = () => {
           </div>
         </a>
       </Timeline.Item>
-      <Timeline.Item color="purple">
+      <Timeline.Item color="#F4F4F2">
         <p className="font-black border-matte border-b-2">RUNTEQ web development learning 2021-04-16</p>
       </Timeline.Item>
-      <Timeline.Item color="red">
+      <Timeline.Item color="#F4F4F2">
         Promoted to Assistant Manager.
         As assistant manager, management duties. Created sales floor, managed inventory, sales, hiring, and training plans for 22 employees, maximized store profit and cost efficiency, and managed the store. 2021-11-30
       </Timeline.Item>
-      <Timeline.Item color="red">
+      <Timeline.Item color="#F4F4F2">
         Appointed as a regular employee.
         As acting store manager, created sales floor, inventory control, sales planning, and trained 5 subordinates. 2019-02-23
       </Timeline.Item>
-      <Timeline.Item color="red"><p className="font-black border-matte border-b-2">G.U. Corporation 2016-02-23</p></Timeline.Item>
+      <Timeline.Item color="#F4F4F2"><p className="font-black border-matte border-b-2">G.U. Corporation 2016-02-23</p></Timeline.Item>
       <Timeline.Item>Ohgiya East Japan Co. 2014-04-15</Timeline.Item>
-      <Timeline.Item color="green" className="bg-white">
+      <Timeline.Item color="#F4F4F2" className="bg-white">
         Fukushima Prefectural Sukagawa High School 2014-3-12
       </Timeline.Item>
     </Timeline>
