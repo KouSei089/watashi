4e9d67ed59a4179b0fd17ccc5e8c6342227b9ade
KouSei089 <qq00891829@icloud.com>
2023-05-17 11:53:47 +0900

    add:Note.jsの追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import 'antd/dist/antd.min.css';
 import './App.css';
 import Profile from './Profile';
 import History from './History';
-import Portfolio from './Portfolio';
-import Output from './Output';
 import Contact from './Contact';
+import Note from './Note';
+import axios from 'axios';
 
 function App() {
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   //fetch("")https://note.com/api/v2/creators/izuha0/contents?kind=note&page=1
+  //   fetch("https://note.com/api/v2/creators/izuha0/", {method: 'get'})
+  //     .then((res) => res.json())
+  //     .then((json) => setData(json))
+  //     .catch(() => alert("error"));
+  // }, []);
+  // console.log(data)
+
+  // useEffect(() => {
+  //   const getUser = async () => {
+  //     const response = await axios.get('https://api.github.com/users/tagty');
+  //     console.log(response.data)
+  //   }
+  //   getUser()
+  // }, [])
+  
   return (
     <div className="bg-natural">
       <Profile />
