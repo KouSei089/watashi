e87bcc0fc2376437aabd695c5f072fcf7d89e1f1
KouSei089 <qq00891829@icloud.com>
2023-07-01 16:14:56 +0900

    update:ディレクトリ構成変更

--- src/about/About.js
+++ src/about/About.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Profile from './Profile';
 import History from './History';
-import Contact from './Contact';
 
 const About = () => {
   return (
     <>
       <Profile />
       <History />
-      <Contact />
     </>
   )
 }
