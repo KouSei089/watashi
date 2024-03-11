964ab615f9495d855317a62ed60496a38520f230
KouSei089 <qq00891829@icloud.com>
2024-03-11 20:35:52 +0900

    update:GASを使用して、noteのデータを取得できるよう修正

--- src/book/Book.js
+++ src/book/Book.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useCallback } from "react";
-import booksData from "./book-data.json";
+// import booksData from "./book-data.json";
+import books from "./book-GAS.json";
 
 const BookCard = ({ src, title, date, link }) => (
   <Link to={link} className="block inline-block px-8 py-12">
@@ -13,22 +14,23 @@ const BookCard = ({ src, title, date, link }) => (
 );
 
 const Book = () => {
-  const [displayItemsCount, setDisplayItemsCount] = useState(6);
+  const [displayItemsCount, setDisplayItemsCount] = useState(12);
   const loadMore = useCallback(() => {
-    setDisplayItemsCount((count) => count + 6);
+    setDisplayItemsCount((count) => count + 12);
   }, []);
 
-  const isLoading = displayItemsCount < booksData.length;
-  const displayItems = booksData.slice(0, displayItemsCount);
+  const isLoading = displayItemsCount < books.length;
+  const displayItems = books.slice(0, displayItemsCount);
 
   return (
-    <div className="px-20 lg:px-72  text-matte">
+    <div className="">
+      <div className="px-20 lg:px-72 text-matte text-center">
       {displayItems.map((book) => (
         <BookCard
           key={book.title}
-          src={book.src}
-          title={book.title}
-          date={book.date}
+          src={book.eyecatch}
+          title={book.name}
+          date={book.created_at}
           link={book.noteUrl}
         />
       ))}
@@ -42,6 +44,7 @@ const Book = () => {
           </button>
         </div>
       )}
+      </div>
     </div>
   );
 };
