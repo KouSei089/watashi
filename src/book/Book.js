e964cad30b62db6a7572783f02194c83d97c9396
KouSei089 <qq00891829@icloud.com>
2024-03-11 20:49:14 +0900

    fix:noteのリンクが壊れているバグ対応

--- src/book/Book.js
+++ src/book/Book.js
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
 import { useState, useCallback } from "react";
-// import booksData from "./book-data.json";
 import books from "./book-GAS.json";
 
 const BookCard = ({ src, title, date, link }) => (
-  <Link to={link} className="block inline-block px-8 py-12">
+  <a href={link} target="_blank" className="block inline-block px-8 py-12">
     <div className="transform transition-all duration-300 hover:opacity-50 font-body">
       <img className="w-72 h-48 object-cover" src={src} alt={title} />
       <h2 className="mt-4 text-base text-sky-gray">{title}</h2>
       <h3 className="mt-2 text-light-gray">{date}</h3>
     </div>
-  </Link>
+  </a>
 );
 
 const Book = () => {
